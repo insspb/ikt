a9821ef9245f5a94258163f5840770f3f7cddcd3
Andrey Shpak <insspb@users.noreply.github.com>
2020-05-10 01:00:02 +0300

    Added: Pytest tests and nox workflows, including linting (#21)
    * Pytest configuration and simple test #19
    
    * Nox CI/CD configuration #19
    
    * Nox GitHub actions workflow #19
    
    * Nox GitHub actions workflow limits #19
    
    * Remove coverage step #19

---
+++ noxfile.py
@@ -0,0 +1,42 @@
+"""Nox tool configuration file.
+
+Nox is Tox tool replacement.
+"""
+import nox
+
+
+nox.options.keywords = "not docs"
+
+
+@nox.session(python=["3.7", "3.8"])
+def tests(session):
+    """Run test suite with pytest."""
+    session.install("-r", "requirements.txt")
+    session.install("-e", ".")
+    session.run("pytest")
+
+
+@nox.session(python="3.8")
+def linting(session):
+    """Launch linting locally."""
+    session.install("-r", "requirements.txt")
+    session.install("-e", ".")
+    session.run("pre-commit", "run", "-a")
+
+
+@nox.session(python="3.8")
+def docs(session):
+    """Build the documentation."""
+    session.run("rm", "-rf", "docs/_build", external=True)
+    session.install("-r", "docs/requirements.txt")
+    session.install(".")
+    session.cd("docs")
+    sphinx_args = ["-b", "html", "-W", "source", "build/html"]
+
+    if not session.interactive:
+        sphinx_cmd = "sphinx-build"
+    else:
+        sphinx_cmd = "sphinx-autobuild"
+        sphinx_args.insert(0, "--open-browser")
+
+    session.run(sphinx_cmd, *sphinx_args)
